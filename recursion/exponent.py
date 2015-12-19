ff45476cd8dba23bd6f825cb9146e345169d8708
ishank <ishank296@gmail.com>
2015-12-19 22:55:24 +0530

    effecient exponent

--- recursion/exponent.py
+++ recursion/exponent.py
@@ -3,3 +3,14 @@ def exponent(k,n):
         return 1
     else:
         return k * exponent(k,n-1) 
+
+        
+def recursion2(k,n):
+    if n==0:
+        return 1
+    else:
+        ans = recursion2(k,n/2)
+        if n%2 == 0:
+            return ans * ans
+        else:
+            return ans * ans * k
